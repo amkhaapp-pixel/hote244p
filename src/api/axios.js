32c4869574387ea79a7036ca26be35be04cb2678
Amkha <amkhaapp@gmail.com>
2026-05-24 17:34:06 +0800

    add  src/api/axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://hote244-api.onrender.com/api',
+  baseURL: 'http://localhost:5000/api',
 });
 
 // Add interceptor to include token in requests
